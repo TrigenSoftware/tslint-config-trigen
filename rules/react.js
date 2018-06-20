1ba8b7f54344942597d2a785cf41d81e4b4a1f59
Dan Green <danon0404@gmail.com>
2018-06-21 03:23:33 +0700

    v3.0.2

--- rules/react.js
+++ rules/react.js
@@ -15,7 +15,7 @@ module.exports = {
 		'jsx-equals-spacing': ['error', 'never'],
 		'jsx-key':             'error',
 		'jsx-no-bind':         'error',
-		'jsx-no-lambda':       'off',
+		'jsx-no-lambda':       'error',
 		'jsx-no-string-ref':   'error',
 		'jsx-self-close':      'error',
 		'jsx-wrap-multiline':  'error',
