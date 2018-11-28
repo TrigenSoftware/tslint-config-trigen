b1a68904c9c113830d4359c2444beae6bd6d891e
dangreen <danon0404@gmail.com>
2018-11-28 07:21:26 +0700

    test fix

--- test/test.sh
+++ test/test.sh
@@ -3,7 +3,7 @@
 output=$(tslint -p tsconfig.json valid.ts)
 
 if [[ $output == *'ERROR'* ]]; then
-	echo "valid.js doesn't passed checking."
+	echo "valid.ts doesn't passed checking."
 	echo "$output"
 	exit 1
 fi
@@ -17,7 +17,7 @@ fi
 output=$(tslint -p tsconfig.json invalid.ts)
 
 if [[ $output != *'ERROR'* ]]; then
-	echo 'invalid.js passed checking.'
+	echo 'invalid.ts passed checking.'
 	echo "$output"
 	exit 1
 fi
